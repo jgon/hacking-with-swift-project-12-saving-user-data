fbaec415e3ad85e3a7129264cc5f76dc3ace21a7
JackG <jacques.gonzalez@me.com>
2016-02-13 17:48:29 +0100

    Initial Commit

---
+++ Project12/ViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  Project12
+//
+//  Created by Jacques on 11/02/16.
+//  Copyright © 2016 J4SOFT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    var people = [Person]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Person", forIndexPath: indexPath) as! PersonCollectionViewCell
+        
+        let person = people[indexPath.item]
+        
+        cell.nameLabel.text = person.name
+        let imageFilename = getDocumentsDirectory().stringByAppendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: imageFilename)
+        
+        cell.imageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).CGColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let person = people[indexPath.item]
+        
+        let alertController = UIAlertController(title: "Rename person", message: nil, preferredStyle: .Alert)
+        
+        alertController.addTextFieldWithConfigurationHandler(nil)
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { [unowned self, alertController] (UIAlertAction) -> Void in
+            let newName = alertController.textFields![0]
+            person.name = newName.text!
+            self.collectionView.reloadData()
+        }))
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+
+    @IBAction func addNewPerson(sender: UIBarButtonItem) {
+        let pickerController = UIImagePickerController()
+        pickerController.allowsEditing = true
+        pickerController.delegate = self
+        presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        var newImage: UIImage
+        
+        if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            newImage = possibleImage
+        } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            newImage = possibleImage
+        } else {
+            return
+        }
+        
+        let imageFilename = NSUUID().UUIDString
+        let imagePath = getDocumentsDirectory().stringByAppendingPathComponent(imageFilename)
+        
+        if let jpegData = UIImageJPEGRepresentation(newImage, 80) {
+            jpegData.writeToFile(imagePath, atomically: true)
+        }
+        
+        let person = Person(name: "Unknown", image: imageFilename)
+        people.append(person)
+        collectionView.reloadData()
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func getDocumentsDirectory() -> NSString {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# Hacking with Swift - Project 12 - UICollectionViews
+
+How to save user settings and data for later use.
+
+## Setup
+
+- Xcode 7.2.1
+- Swift 2.0
+- iOS 9.2
+- iPad
+
+## Topics covered
